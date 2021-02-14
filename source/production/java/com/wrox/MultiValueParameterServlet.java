997b6bf65a875329f8185c9aea5533840b326e4a
fanli.bu <bflbfl1980@163.com>
2021-02-14 20:20:00 +0800

    add checkboxes

---
+++ source/production/java/com/wrox/MultiValueParameterServlet.java
@@ -0,0 +1,52 @@
+package com.wrox;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+		name = "multiValueParameterServlet",
+		urlPatterns = {"/checkboxes"})
+public class MultiValueParameterServlet extends HttpServlet {
+
+	/**
+	 * add serial version
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException,IOException{
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		
+		PrintWriter pw = response.getWriter();
+		pw.append("<!DOCTYPE html>\r\n")
+		  .append("<html>\r\n")
+		  .append("    <head>\r\n")
+		  .append("        <title>Hello,user application</title>\r\n")
+		  .append("    </head>\r\n")
+		  .append("    <body>\r\n")
+		  .append("        <form action=\"checkboxes\" method=\"post\">\r\n")
+		  .append("Select fruits you like to eat</br>\r\n")
+		  .append("<input type=\"checkbox\" name=\"fruit\" value=\"Banana\" />\r\n")
+		  .append("banana</br>\r\n")
+          .append("<input type=\"checkbox\" name=\"fruit\" value=\"Apple\" />\r\n")
+		  .append("Apple</br>\r\n")
+		  .append("<input type=\"checkbox\" name=\"fruit\" value=\"Orange\" />\r\n")
+		  .append("Orange</br>\r\n")
+		  .append("<input type=\"checkbox\" name=\"fruit\" value=\"Guava\" />\r\n")
+		  .append("Guava</br>\r\n")
+		  .append("<input type=\"checkbox\" name=\"fruit\" value=\"Kiwi\" />\r\n")
+		  .append("Kiwi</br>\r\n")
+		  .append("<input type=\"submit\" value=\"submit\">\r\n")
+		  .append("</form>\r\n")
+		  .append("</body>\r\n")
+		  .append("</html>\r\n");
+	}
+
+}
